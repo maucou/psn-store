38b7e2522f52e1da3ab130b395d803ce8b2db3f7
maucou <maucou123@hotmail.com>
2024-01-15 08:26:08 -0300

    Stylized card-pricing and card-label components

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
   styleUrls: ['./card-pricing.component.css']
 })
-export class CardPricingComponent {
+export class CardPricingComponent implements OnInit {
 
+  @Input()
+  gameMessage: string = ''
+
+  @Input()
+  gameType: string = ''
+
+  @Input()
+  gamePrice: string = ''
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
